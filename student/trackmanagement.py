Fix P: Covariance initialisation

student/trackmanagement.py
@@ -45,12 +45,15 @@ class Track:
                         [ 0.        ],
                         [ 0.        ]])
 
-        self.P = np.matrix([[9.0e-02, 0.0e+00, 0.0e+00, 0.0e+00, 0.0e+00, 0.0e+00],
-                        [0.0e+00, 9.0e-02, 0.0e+00, 0.0e+00, 0.0e+00, 0.0e+00],
-                        [0.0e+00, 0.0e+00, 6.4e-03, 0.0e+00, 0.0e+00, 0.0e+00],
-                        [0.0e+00, 0.0e+00, 0.0e+00, 2.5e+03, 0.0e+00, 0.0e+00],
-                        [0.0e+00, 0.0e+00, 0.0e+00, 0.0e+00, 2.5e+03, 0.0e+00],
-                        [0.0e+00, 0.0e+00, 0.0e+00, 0.0e+00, 0.0e+00, 2.5e+01]])
+        P_pos = M_rot * meas.R * M_rot.T
+        P_vel = np.matrix([
+            [params.sigma_p44**2, 0, 0],
+            [0, params.sigma_p55**2, 0],
+            [0, 0, params.sigma_p66**2]
+        ])
+        self.P = np.zeros((6, 6))
+        self.P[:3,:3] = P_pos
+        self.P[3:,3:] = P_vel
         
         self.state = 'initialised'
         self.score = 1/params.window
@@ -120,9 +123,9 @@ class Trackmanagement:
         for i in unassigned_tracks:
             track = self.track_list[i]
 
-            if (track.state == "initialised" and track.score < 0.2 and track.age>10) or \
+            if (track.state == "initialised" and track.score < 0.2 and track.age>params.stablization_age) or \
                 (track.state == "tentative" and track.score < 0.3) or \
-                (track.state == "confirmed" and track.score < 0.6) or\
+                (track.state == "confirmed" and track.score < params.delete_threshold) or\
                 (track.P[0,0]>params.max_P) or (track.P[1,1]>params.max_P):
                 tracks_to_be_deleted.append(track)
         
@@ -160,10 +163,10 @@ class Trackmanagement:
 
         track.score += 1/params.window
         track.score = min(track.score, 1)
-        if track.state == "initialised" and track.score > 0.4:
+        if track.state == "initialised" and track.score > params.tentative_threshold:
             track.state = "tentative"
             
-        if track.state == "tentative" and track.score > 0.6:
+        if track.state == "tentative" and track.score > params.confirmed_threshold:
             track.state = "confirmed"
         
         ############